6a35369fc5452489d4509bc60a20ba585a339745
Ashwin Jain <ashwinjain@Rinkis-MacBook-Pro.local>
2023-06-18 11:37:04 -0500

    button stays blue, hover is broken

--- script.js
+++ script.js
@@ -2,15 +2,27 @@
 document.getElementById("defaultOpen").style.display = "block";
 
 // function to deal with tab onClicks
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
 
+function openTabTest(thisId) {
+    
     // get tabcontent elements
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
+    var tabcontent = document.getElementsByClassName("tabcontent");
+
+    // hide all tabcontent
+    for (var i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
+        document.getElementById(thisId).style.backgroundColor = "#2b3b54";
     }
 
+    // change all tabs to orange
+    var tabs = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.backgroundColor = "#FF5F05";
+    }
+
+    var tabName = thisId.replace("_button", "")
+    
     document.getElementById(tabName).style.display = "block";
+    document.getElementById(thisId).style.backgroundColor = "#2b3b54";
 
 }
